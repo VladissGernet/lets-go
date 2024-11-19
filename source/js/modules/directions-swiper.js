4cbccd5b971313a4067a533a024e1351475cbedb
VladissGernet <skeimorr@gmail.com>
2024-11-19 21:32:14 +0400

    feat: отклил свайпер на mobile

--- source/js/modules/directions-swiper.js
+++ source/js/modules/directions-swiper.js
@@ -1,14 +1,20 @@
 import {Swiper} from '../vendor/swiper.min';
 
+import {debounce} from '../vendor/debounce.js';
+
 const SLIDES_PER_VIEW = 3;
 const SPACE_BETWEEN = 10;
 const AUTOPLAY_DELAY = 3000;
-const SwiperBreakpoints = {
-  MOBILE: 300,
-  TABLET: 768,
-};
+const PAGE_RELOAD_DELAY = 1000;
+const TABLET_WIDTH = 768;
 
-const initDirectionsSwiper = () => {
+const onPageResize = () => {
+  location.reload();
+};
+const throttledPageResizing = debounce(onPageResize, PAGE_RELOAD_DELAY);
+
+// Функция для создания экземпляра Swiper
+const createSwiper = () => {
   return new Swiper('.directions__swiper', {
     wrapperClass: 'directions__swiper-wrapper',
     slideClass: 'directions__slide',
@@ -25,15 +31,30 @@ const initDirectionsSwiper = () => {
       delay: AUTOPLAY_DELAY,
       disableOnInteraction: false,
     },
-    breakpoints: {
-      [SwiperBreakpoints.TABLET]: {
-        enabled: true,
-      },
-      [SwiperBreakpoints.MOBILE]: {
-        enabled: false,
-      },
-    },
   });
+};
+
+const initDirectionsSwiper = () => {
+  const isSwiperOnPage = document.querySelector('.directions__swiper');
+  if (!isSwiperOnPage) {
+    return;
+  }
+
+  // Проверка на ширину экрана
+  // Swiper активизируется только на desktop и tablet разметке
+  if (window.innerWidth >= TABLET_WIDTH) {
+    createSwiper();
+  }
+  const initialPageWidth = window.innerWidth;
+  // Проверка на изменение ширины экрана
+  window.addEventListener('resize', () => {
+    const newPageWidth = window.innerWidth;
+
+    if (initialPageWidth < TABLET_WIDTH && newPageWidth >= TABLET_WIDTH || initialPageWidth >= TABLET_WIDTH && newPageWidth < TABLET_WIDTH) {
+      throttledPageResizing();
+    }
+  });
+
 };
 
 export {initDirectionsSwiper};

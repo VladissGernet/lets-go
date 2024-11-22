58915d52817def52a4259c379c0e1c630e4c0d5a
VladissGernet <skeimorr@gmail.com>
2024-11-22 19:26:31 +0400

    feat: создает кнопки фильтра

--- source/js/modules/catalog/filter.js
+++ source/js/modules/catalog/filter.js
@@ -110,17 +110,46 @@ const countriesByContinent = {
   ],
 };
 
+const INITIAL_ACTIVATED_CONTINENT = 'Европа';
+
+const createFilterContinentsButtons = (filter) => {
+  if (!filter) {
+    return;
+  }
+
+  const filterContinents = filter.querySelector('.filter__continents');
+  const filterContinentButtonCover = filterContinents.querySelector('.filter__continent');
+  const filterContinentButtonClone = filterContinentButtonCover.cloneNode(true);
+
+  const continentsDocumentFragment = document.createDocumentFragment();
+
+  Object.keys(countriesByContinent).forEach((continent) => {
+    const newItem = filterContinentButtonClone.cloneNode(true);
+    newItem.querySelector('button').textContent = continent;
+
+    if (continent === INITIAL_ACTIVATED_CONTINENT) {
+      newItem.classList.add('filter__continent--active');
+    }
+    continentsDocumentFragment.appendChild(newItem);
+  });
+  filterContinents.innerHTML = '';
+
+  filterContinents.appendChild(continentsDocumentFragment);
+};
+
 const initCountriesFiler = () => {
   const filter = document.querySelector('.filter');
   if (!filter) {
     return;
   }
-  const filterContent = filter.querySelector('.filter__content');
-  console.log(filterContent);
+  createFilterContinentsButtons(filter);
 
-  // Создать список из континентов
+
+
+  // Создать список из кнопок с континентами
   const continents = Object.keys(countriesByContinent);
   console.log(continents);
+
 
 
   // Первому континенту добавить класс active

b9bf3d9e9fdc2010184989832b0c69db0b1df7af
VladissGernet <skeimorr@gmail.com>
2024-11-23 10:03:56 +0400

    feat: Первичная разметка планшета

--- source/js/modules/catalog/create-tablet-alphabet.js
+++ source/js/modules/catalog/create-tablet-alphabet.js
@@ -1,12 +1,44 @@
+const createListOfCountries = (countriesArray) => {
+  const listFragment = document.createDocumentFragment();
+
+  countriesArray.forEach((country) => {
+    const newCountryItem = document.createElement('li');
+    const newCountryButton = document.createElement('button');
+    newCountryButton.type = 'button';
+    newCountryButton.textContent = country;
+    newCountryItem.appendChild(newCountryButton);
+    listFragment.appendChild(newCountryItem);
+  });
+
+  return listFragment;
+};
+
 const createTabletAlphabet = (tabletContent, sortedCountries) => {
   const alphabetList = tabletContent.querySelector('.filter__tablet-alphabet');
   const selectedLetterContentList = tabletContent.querySelector('.filter__selected-letter-content');
-  console.log(alphabetList);
-  console.log(selectedLetterContentList);
-  alphabetList.style.height = "100px";
-  alphabetList.style.background = "tomato";
 
+  // Создаем список букв из алфавита и список стран по первой букве.
 
+  const alphabetListFragment = document.createDocumentFragment();
+  let selectedLetterContentListFragment;
+
+  // Создаем элементы букв
+  sortedCountries.forEach((letterArray) => {
+    const [letter] = letterArray;
+    const newLetterItem = document.createElement('li');
+    const newLetterButton = document.createElement('button');
+    newLetterButton.type = 'button';
+    newLetterButton.textContent = letter;
+
+    newLetterItem.appendChild(newLetterButton);
+    alphabetListFragment.appendChild(newLetterItem);
+  });
+
+  // Создаем элементы списка стран по первой букве
+  selectedLetterContentListFragment = createListOfCountries(sortedCountries[0][1]);
+
+  alphabetList.appendChild(alphabetListFragment);
+  selectedLetterContentList.appendChild(selectedLetterContentListFragment);
 };
 
 export {createTabletAlphabet};

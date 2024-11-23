2d88dd2ddcf61b961d572dcc1b0ff96d80ca011d
VladissGernet <skeimorr@gmail.com>
2024-11-23 14:34:39 +0400

    feat: функционал фильтра добавлен

--- source/js/modules/catalog/init-hide-button.js
+++ source/js/modules/catalog/init-hide-button.js
@@ -13,11 +13,13 @@ const initHideButton = () => {
 
   const filterContent = document.querySelector('.filter__content');
   const buttonText = hideButton.querySelector('.filter__hide-button-text');
+  const tabletContentWrapper = document.querySelector('.filter__tablet-content-wrapper');
 
   hideButton.addEventListener('click', () => {
     if (filterContent.classList.contains('js-is-open')) {
       filterContent.classList.remove('js-is-open');
       hideButton.classList.remove('js-is-open');
+      tabletContentWrapper.classList.remove('js-is-open');
       buttonText.textContent = FilterHideButtonText.CLOSE;
       filterContent.style.maxHeight = 0;
     } else {
@@ -26,6 +28,7 @@ const initHideButton = () => {
 
       filterContent.classList.add('js-is-open');
       hideButton.classList.add('js-is-open');
+      tabletContentWrapper.classList.add('js-is-open');
       buttonText.textContent = FilterHideButtonText.OPEN;
       filterContent.style.maxHeight = `${filterContent.scrollHeight}px`;
     }

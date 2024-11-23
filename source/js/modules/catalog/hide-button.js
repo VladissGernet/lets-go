bec5b3e70d1bbd8f75f9ed9d0bea9ad4b77f5926
VladissGernet <skeimorr@gmail.com>
2024-11-23 06:46:51 +0400

    feat: desktop done

--- source/js/modules/catalog/hide-button.js
+++ source/js/modules/catalog/hide-button.js
@@ -1,3 +1,8 @@
+const FilterHideButtonText = {
+  OPEN: 'Свернуть',
+  CLOSE: 'Показать все',
+};
+
 const initHideButton = () => {
   const hideButton = document.querySelector('.filter__hide-button');
   if (!hideButton) {
@@ -5,12 +10,18 @@ const initHideButton = () => {
   }
 
   const filterContent = document.querySelector('.filter__content');
+  const buttonText = hideButton.querySelector('.filter__hide-button-text');
+
   hideButton.addEventListener('click', () => {
     if (filterContent.classList.contains('js-is-open')) {
       filterContent.classList.remove('js-is-open');
+      hideButton.classList.remove('js-is-open');
+      buttonText.textContent = FilterHideButtonText.OPEN;
       filterContent.style.maxHeight = 0;
     } else {
       filterContent.classList.add('js-is-open');
+      hideButton.classList.add('js-is-open');
+      buttonText.textContent = FilterHideButtonText.CLOSE;
       filterContent.style.maxHeight = `${filterContent.scrollHeight}px`;
     }
   });
